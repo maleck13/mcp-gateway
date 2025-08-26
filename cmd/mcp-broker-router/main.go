0861b41fb4ac4d2445565160169d26e60010e10c
craig <cbrookes@redhat.com>
2025-08-26 08:08:53 +0100

    example graceful shutdown

--- cmd/mcp-broker-router/main.go
+++ cmd/mcp-broker-router/main.go
@@ -2,11 +2,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	extProcV3 "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
@@ -22,8 +24,42 @@ func getEnv(key, fallback string) string {
 }
 
 func main() {
-	mux := http.NewServeMux()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	brokerServer := setUpBroker()
+	routerServer := setUpRouter()
 
+	grpcAddr := getEnv("SERVER_ADDRESS", "0.0.0.0:9002")
+	lis, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("[grpc] listen error: %v", err)
+	}
+
+	go func() {
+		log.Printf("[grpc] starting MCP Router listening on %s", grpcAddr)
+		log.Fatal(routerServer.Serve(lis))
+	}()
+
+	go func() {
+		log.Printf("[http] starting MCP Broker listening on %s", brokerServer.Addr)
+		if err := brokerServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("[http] %v", err)
+		}
+	}()
+
+	<-stop
+	// handle shutdown
+	log.Printf("shutting down MCP Broker and MCP Router")
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+	if err := brokerServer.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+	routerServer.GracefulStop()
+}
+
+func setUpBroker() *http.Server {
+	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		_, _ = fmt.Fprint(w, "Hello, World!")
 	})
@@ -33,21 +69,11 @@ func main() {
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-	go func() {
-		log.Printf("[http] listening on %s", httpSrv.Addr)
-		if err := httpSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("[http] %v", err)
-		}
-	}()
+	return httpSrv
+}
 
-	grpcAddr := getEnv("SERVER_ADDRESS", "0.0.0.0:9002")
-	lis, err := net.Listen("tcp", grpcAddr)
-	if err != nil {
-		log.Fatalf("[grpc] listen error: %v", err)
-	}
+func setUpRouter() *grpc.Server {
 	grpcSrv := grpc.NewServer()
 	extProcV3.RegisterExternalProcessorServer(grpcSrv, &mcpRouter.ExtProcServer{})
-
-	log.Printf("[grpc] listening on %s", grpcAddr)
-	log.Fatal(grpcSrv.Serve(lis))
+	return grpcSrv
 }
